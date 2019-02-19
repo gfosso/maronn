233862b953721b2c15c21b4c87a3dfb790dc15cb
Gianluca Lagnese <glagnese@sissa.it>
2019-02-19 14:48:30 +0100

    mps copied from the lattice

--- mps.py
+++ mps.py
@@ -5,7 +5,7 @@ import warnings
 class mps:
 
 
-    def __init__(self,site_dimension=2,bond_dimension=100):
+    def __init__(self,site_dimension=2,bond_dimension=200):
         self.site_dimension = site_dimension
         self.bond_dimension = bond_dimension
         self.product_state()
@@ -46,30 +46,31 @@ class mps:
         self._Lambda=lambd
 
     def product_state(self,neel=False):
-        """neel=True to inizialize as a neel state"""
         self.Gamma=[]
         self.Lambda=[]
         d=self.site_dimension
         for i in range(2):
-                self.Lambda.append(np.ones([1]))
-                self.Gamma.append(np.zeros([d,1,1]))
+            self.Lambda.append(np.ones([1]))
+            self.Gamma.append(np.zeros([d,1,1]))
         if neel:
-                self.Gamma[0][0,0,0]=1
-                self.Gamma[1][d-1,0,0]=1
+            self.Gamma[0][0,0,0]=1
+            self.Gamma[1][d-1,0,0]=1
         else:
-                self.Gamma[0][0,0,0]=1
-                self.Gamma[1][0,0,0]=1
+            self.Gamma[0][0,0,0]=1
+            self.Gamma[1][0,0,0]=1
 
-    def evol(self,U):
+    def evol(self,Ua,Ub):
         d=self.site_dimension
         chi=self.bond_dimension
-        U=np.reshape(U,(d,d,d,d))
+        U=[]
+        U.append(np.reshape(Ua,(d,d,d,d)))
+        U.append(np.reshape(Ub,(d,d,d,d)))
         for i_bond in [0,1]:
             ia = np.mod(i_bond-1,2); ib = np.mod(i_bond,2); ic = np.mod(i_bond+1,2)
             chia = self.Gamma[ib].shape[1]; chic = self.Gamma[ic].shape[2]
             # Construct theta matrix and time evolution #
             theta = np.tensordot(self.Gamma[ib],self.Gamma[ic],axes=(2,1)) # i a j b
-            theta = np.tensordot(U,theta,axes=([2,3],[0,2])) # ip jp a b 
+            theta = np.tensordot(U[i_bond],theta,axes=([2,3],[0,2])) # ip jp a b 
             theta = np.tensordot(np.diag(self.Lambda[ia]),theta,axes=([1,2])) # a ip jp b 
             theta = np.reshape(np.transpose(theta,(1,0,2,3)),(d*chia,d*chic)) # ip a jp b
             # Schmidt decomposition #
